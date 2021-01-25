7fb7633e90637a1e528b64245668e4d7e1018bae
Marco Parrone <marco@marcoparrone.com>
2021-01-25 01:18:07 +0100

    Added key to options. Removed debug message.

--- src/index.js
+++ src/index.js
@@ -7,11 +7,9 @@ const defaultSupportedLanguages = require ('./supported-languages.json');
 export default class LanguageSelector extends React.Component {
     render() {
         let supportedLanguages = (this.props.supportedLanguages) ? this.props.supportedLanguages : defaultSupportedLanguages;
-        let returnV  = React.createElement('div', null,
-                            React.createElement('label', {htmlFor: "LanguageSelector"}, this.props.text_language),
-                            React.createElement('select', {id: "LanguageSelector", name: "lang", lang: "en", value: this.props.language, onChange: this.props.handleSettingsChange},
-                                supportedLanguages.map((langdesc) => {return React.createElement('option',{value: langdesc.code},langdesc.name)})));
-    console.log(returnV);
-    return returnV;
+        return React.createElement('div', null,
+                 React.createElement('label', {htmlFor: "LanguageSelector"}, this.props.text_language),
+                   React.createElement('select', {id: "LanguageSelector", name: "lang", lang: "en", value: this.props.language, onChange: this.props.handleSettingsChange},
+                     supportedLanguages.map((langdesc) => {return React.createElement('option',{value: langdesc.code, key: langdesc.code},langdesc.name)})));
     }
 }
